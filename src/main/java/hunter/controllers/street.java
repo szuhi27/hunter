667aa1d0e0971dfabd9bf25d97eba408c816490e
Nagy Viktor <nagy.viktor6@gmail.com>
2020-11-12 13:11:14 +0100

    initial body() method created

--- src/main/java/hunter/controllers/street.java
+++ src/main/java/hunter/controllers/street.java
@@ -1,10 +1,15 @@
 package hunter.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 import java.util.Random;
 
 public class street {
 
     private int whichEnemy;
+    private Pane thisEnemy;
 
     public  void random() {
         int lastEnemy = whichEnemy;
@@ -15,4 +20,9 @@ public class street {
         }
     }
 
+    public void body(ActionEvent event) {
+
+        thisEnemy.setVisible(false);
+
+    }
 }
