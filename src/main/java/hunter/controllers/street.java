140884b0ab2be6c3668b737386860672d346a808
Nagy Viktor <nagy.viktor6@gmail.com>
2020-11-12 13:04:07 +0100

    random() method created

--- src/main/java/hunter/controllers/street.java
+++ src/main/java/hunter/controllers/street.java
@@ -1,5 +1,18 @@
 package hunter.controllers;
 
+import java.util.Random;
+
 public class street {
+
+    private int whichEnemy;
+
+    public  void random() {
+        int lastEnemy = whichEnemy;
+        Random random = new Random();
+        whichEnemy = random.nextInt(7);
+        if (whichEnemy == lastEnemy){
+            random();
+        }
+    }
 
 }
