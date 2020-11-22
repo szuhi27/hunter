4c1151c33ff52c3de693d92763d3285371d16ee0
David <szuhid@gmail.com>
2020-11-23 00:33:20 +0100

    HighScore variables

--- src/main/java/hunter/controllers/HighScore.java
+++ src/main/java/hunter/controllers/HighScore.java
@@ -1,19 +1,38 @@
 package hunter.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HighScore {
 
-    //azért kell bele hogy át lehessen lépni a highscore oldalra, funkciók később
+    @FXML
+    private ImageView hsBcg,muteIV,loadIV,menuButtonIV,clearIV;
+    @FXML
+    private TextArea textArea,placements;
+    @FXML
+    private ChoiceBox<String> choiceBox;
+    @FXML
+    private Text titles;
+    @FXML
+    private Pane confirmPane;
 
-    //javafxben is hozzáadni a methodokat, onnan kiszedtem hogy fussék
+    private MediaPlayer a;
+    private boolean isThereData = false, music=true;
+    private String whichMap;
+
 
     public void menuGoGo(ActionEvent event) throws IOException {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
