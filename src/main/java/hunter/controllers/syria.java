3ca56d2f3222a27657aa6fe2c6ebe7b1aeb5c510
Nagy Viktor <nagy.viktor6@gmail.com>
2020-11-28 09:07:20 +0100

    necessary fxml annotations with declarations added

--- src/main/java/hunter/controllers/syria.java
+++ src/main/java/hunter/controllers/syria.java
@@ -1,4 +1,25 @@
 package hunter.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
 public class syria {
+
+    @FXML
+    private Pane one, two, three, four, five, six, seven, nameEnter, results, ingameScore;
+    @FXML
+    private Text scoreT, killedT, missedT, finalTimeT, goodJobT, finalScoreT;
+    @FXML
+    private Label timeL;
+    @FXML
+    private TextField nameTF;
+    @FXML
+    private Button exitB, muteB;
+    @FXML
+    private ImageView syriaBcg,enemyOne,enemyTwo,enemyThree,enemyFour,enemyFive,enemySix,enemySeven;
 }
