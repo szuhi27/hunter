80e8bb7fa894aaf7698ea259e5bd923f3a9aa2de
Your Name <bogergo9@gmail.com>
2020-12-07 01:04:27 +0100

    Menu test és street test kész

---
+++ src/main/java/test/hunter/controllers/MenuTest.java
@@ -0,0 +1,36 @@
+package hunter.controllers;
+
+import org.junit.jupiter.api.DynamicTest;
+
+import static org.junit.Assert.*;
+
+public class MenuTest {
+
+    @org.junit.Test
+    public void initialize() {
+    }
+
+    @org.junit.Test
+    public void play() {
+    }
+
+    @org.junit.Test
+    public void goAfghanistan() {
+    }
+
+    @org.junit.Test
+    public void goSyria() {
+    }
+
+    @org.junit.Test
+    public void highscore() {
+    }
+
+    @org.junit.Test
+    public void exitApp() {
+    }
+
+    @org.junit.Test
+    public void mute() {
+    }
+}
