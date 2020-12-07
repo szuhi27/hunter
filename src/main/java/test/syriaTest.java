ede4899ecc334a37077ef7ca2ddcb44bb3350d4d
Your Name <bogergo9@gmail.com>
2020-12-07 01:05:57 +0100

    Syria test added

---
+++ src/main/java/test/syriaTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class syriaTest {
+
+    @Test
+    public void initialize() {
+    }
+
+    @Test
+    public void start() {
+    }
+
+    @Test
+    public void scoreCheck() {
+    }
+
+    @Test
+    public void enemy() {
+    }
+
+    @Test
+    public void random() {
+    }
+
+    @Test
+    public void win() {
+    }
+
+    @Test
+    public void body() {
+    }
+
+    @Test
+    public void head() {
+    }
+
+    @Test
+    public void missClick() {
+    }
+
+    @Test
+    public void toMenu() {
+    }
+
+    @Test
+    public void mute() {
+    }
+}
